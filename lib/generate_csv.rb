8a0debc4884495a0313d331c46228a42162dde46
Eumir Gaspar <eumir@aelogica.com>
2013-05-28 01:26:11 +0800

    Added totals to csv

--- lib/generate_csv.rb
+++ lib/generate_csv.rb
@@ -2,11 +2,18 @@ module Sinatra
   module GenerateCsv
     def generate_csv
       CSV.open('receipts.csv', 'wb', row_sep: "\r\n") do |csv|
-        csv << ['Date', 'Meals', 'Business Development', 'Parking/Toll','Taxi/Bus','Gas/Fuel','Computer Accessories','Books and Periodicals','Medical','Others']
+        csv << ['Date'] + Receipt::RECEIPT_TYPES
 
         Receipt.all.each do |receipt|
           csv << receipt.as_csv
         end
+
+        last_row = ['Total']
+        Receipt::RECEIPT_TYPES.each_with_index do |receipt_type, index|
+          last_row[index+1] = Receipt.get_total_for(receipt_type)
+        end
+
+        csv << last_row
       end
     end
   end
